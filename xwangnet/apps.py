da79e0b27f85ac0e2d6639dc822e1d494c06a332
asyu <65544947+asyu00@users.noreply.github.com>
2025-02-07 00:27:34 -0800

    feat: add ports to devicetemplate

--- xwangnet/apps.py
+++ xwangnet/apps.py
@@ -2,29 +2,34 @@ import docker
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-
 def populate_webtop(sender, **kwargs):
     from .models import DeviceTemplate
     webtop_image = {
         'name': 'webtop',
         'version': 'latest',
         'image': "lscr.io/linuxserver/webtop:latest",
-        'description': 'webtop client'
+        'description': 'webtop client',
+        'ports': {3000:None}
     }
     device, created = DeviceTemplate.objects.get_or_create(
         name=webtop_image['name'],
         version=webtop_image['version'],
         defaults={
             'image': webtop_image['image'],
-            'description': webtop_image['description']
+            'description': webtop_image['description'],
+            'ports': webtop_image['ports']
         }
     )
     # Pull if not exist
-    if created:
+    if not created:
+        # check if image already in host
         print(f"pulling {device.image}")
         client = docker.from_env()
-        image = client.images.pull(device.image)
-        print(image)
+        try:
+            image = client.images.get(webtop_image['image'])
+        except docker.errors.ImageNotFound:
+            image = client.images.pull(device.image)
+        print("image", image)
 
 class XwangnetConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
