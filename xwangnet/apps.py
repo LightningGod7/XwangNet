ffd0138a127ff9a0d7c75812d05609f20b9d5c18
asyu <65544947+asyu00@users.noreply.github.com>
2025-02-07 16:32:37 +0800

    Webtop enhancements (#43)
    * Auto populate webtop image if not exist when running migrate
    
    * implemented minimum 1 webtop per deployment
    
    * feat: add ports to devicetemplate
    
    * refactor: generatePortMappings to account for arrays

--- xwangnet/apps.py
+++ xwangnet/apps.py
@@ -1,6 +1,42 @@
+import docker
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def populate_webtop(sender, **kwargs):
+    from .models import DeviceTemplate
+    webtop_image = {
+        'name': 'webtop',
+        'version': 'latest',
+        'image': "lscr.io/linuxserver/webtop:latest",
+        'description': 'webtop client',
+        'ports': {3000:None}
+    }
+    device, created = DeviceTemplate.objects.get_or_create(
+        name=webtop_image['name'],
+        version=webtop_image['version'],
+        defaults={
+            'image': webtop_image['image'],
+            'description': webtop_image['description'],
+            'ports': webtop_image['ports']
+        }
+    )
+    # Pull if not exist
+    if not created:
+        # check if image already in host
+        print(f"pulling {device.image}")
+        client = docker.from_env()
+        try:
+            image = client.images.get(webtop_image['image'])
+        except docker.errors.ImageNotFound:
+            image = client.images.pull(device.image)
+        print("image", image)
 
 class XwangnetConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "xwangnet"
+
+
+    def ready(self):
+        post_migrate.connect(populate_webtop, sender=self)
+
+
